92730395a9d4e80905527e1c310124f667e3a80b
Marc Tapp <marc.tapp@gmail.com>
2020-05-21 12:18:38 -0400

    updating knexfile for postgres

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+const pgConnection = process.env.DATABASE_URL || 'postgres://dhfrmjqxivteja:69917255b305bacbfa5dd253f34d6f2ae68c3f62088528638ffee54683861194@ec2-54-86-170-8.compute-1.amazonaws.com:5432/dbeoapuiveup24'
+
 module.exports = {
   development: {
     client: "sqlite3",
@@ -18,6 +20,20 @@ module.exports = {
       filename: "./data/test.db3",
     },
     useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: {
       directory: "./data/migrations",
     },
